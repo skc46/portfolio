e525bc2e4e8fce589420b4817c2ca8fd248c8c13
skc46 <skc@crimson.ua.edu>
2021-03-04 11:39:55 -0700

    issue fixed

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -142,7 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
